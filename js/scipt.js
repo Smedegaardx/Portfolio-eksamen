1c51d463456267a84ddbb89f245d6d620fff3a19
Anthon <anan0013@stud.kea.dk>
2025-06-08 23:30:42 +0200

    Fonts tilføjet. Footer, header og headersection færdiggjort.

---
+++ js/scipt.js
@@ -0,0 +1,7 @@
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty("--scroll", window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  },
+  false
+);
